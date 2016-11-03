b2cf347323f0aec13dd4b1439fc7578f774c480e
Vladimir Iglovikov <iglovikov@gmail.com>
2016-11-02 20:04:02 -0700

    temp commit

---
+++ src/xgb_bayesian_REMOTE_1271.py
@@ -0,0 +1,116 @@
+"""
+Baysian hyperparameter optimization [https://github.com/fmfn/BayesianOptimization]
+for Mean Absoulte Error objective
+on default features for https://www.kaggle.com/c/allstate-claims-severity
+"""
+
+__author__ = "Vladimir Iglovikov"
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error
+from bayes_opt import BayesianOptimization
+from tqdm import tqdm
+import numpy as np
+from scipy.stats import skew, boxcox
+import os
+import xgboost as xgb
+
+
+def evalerror(preds, dtrain):
+    labels = dtrain.get_label()
+    return 'mae', mean_absolute_error(np.exp(preds), np.exp(labels))
+
+
+def xgb_evaluate(min_child_weight,
+                 colsample_bytree,
+                 max_depth,
+                 subsample,
+                 gamma,
+                 alpha):
+
+    params['min_child_weight'] = int(min_child_weight)
+    params['cosample_bytree'] = max(min(colsample_bytree, 1), 0)
+    params['max_depth'] = int(max_depth)
+    params['subsample'] = max(min(subsample, 1), 0)
+    params['gamma'] = max(gamma, 0)
+    params['alpha'] = max(alpha, 0)
+
+    cv_result = xgb.cv(params, xgtrain, num_boost_round=num_rounds, nfold=5,
+                         seed=random_state,
+                         feval=evalerror,
+                         callbacks=[xgb.callback.early_stop(50)])
+
+    return -cv_result['test-mae-mean'].values[-1]
+
+
+def prepare_data():
+    train = pd.read_csv('../data/train.csv')
+
+    y = np.log(train['loss'] + shift)
+
+    print train.info()
+    numerical_feats = train.dtypes[train.dtypes != "object"].index
+    # compute skew and do Box-Cox transformation
+    skewed_feats = train[numerical_feats].apply(lambda x: skew(x.dropna()))
+    print 'Skew in numeric features:'
+    print skewed_feats
+    # transform features with skew > 0.25 (this can be varied to find optimal value)
+    skewed_feats = skewed_feats[skewed_feats > 0.25]
+    skewed_feats = skewed_feats.index
+    for feats in tqdm(skewed_feats):
+        train[feats] += 1
+        train[feats], lam = boxcox(train[feats])
+
+    categorical_columns = train.select_dtypes(include=['object']).columns
+
+    for column in tqdm(categorical_columns):
+        le = LabelEncoder()
+        train[column] = le.fit_transform(train[column])
+
+    X = train.drop(['loss', 'id'], 1)
+    xgtrain = xgb.DMatrix(X, label=y)
+
+    return xgtrain
+
+
+if __name__ == '__main__':
+    num_rounds = 10000
+    random_state = 2016
+    num_iter = 10
+    init_points = 10
+    shift = 0
+
+    xgtrain = prepare_data()
+
+    params = {
+        'eta': 0.1,
+        'silent': 1,
+        'eval_metric': 'mae',
+        'verbose_eval': True,
+        'seed': random_state
+    }
+
+    previous_points = pd.read_csv('params/parameters.csv')
+
+    xgbBO = BayesianOptimization(xgb_evaluate, {'min_child_weight': (1, 5),
+                                                'colsample_bytree': (0.3, 0.6),
+                                                'max_depth': (10, 15),
+                                                'subsample': (0.7, 1),
+                                                'gamma': (0, 3),
+                                                'alpha': (0, 3),
+                                                })
+
+    # xgbBO.initialize_df(previous_points)
+
+    xgbBO.maximize(init_points=init_points, n_iter=num_iter)
+
+    # Save results
+    param_cache_path = 'params'
+    try:
+        os.mkdir(param_cache_path)
+    except:
+        pass
+
+    file_name = str(num_rounds) + '_' + str(params['eta']) + '.csv'
+    xgbBO.points_to_csv(file_name)
---
+++ src/xgb_first_level.py
@@ -0,0 +1,136 @@
+import pandas as pd
+
+import sys
+
+sys.path += ['/home/vladimir/packages/xgboost/python-package']
+import xgboost as xgb
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import KFold
+from pylab import *
+
+train = pd.read_csv('../data/train.csv')
+test = pd.read_csv('../data/test.csv')
+test['loss'] = np.nan
+joined = pd.concat([train, test])
+
+
+def evalerror(preds, dtrain):
+    labels = dtrain.get_label()
+    return 'mae', mean_absolute_error(np.exp(preds), np.exp(labels))
+
+
+def logregobj(preds, dtrain):
+    labels = dtrain.get_label()
+    con = 2
+    x =preds-labels
+    grad =con * x / (np.abs(x) + con)
+    hess =con**2 / (np.abs(x) + con)**2
+    return grad, hess
+
+
+for column in list(train.select_dtypes(include=['object']).columns):
+    if train[column].nunique() != test[column].nunique():
+        # Let's find extra categories...
+        set_train = set(train[column].unique())
+        set_test = set(test[column].unique())
+        remove_train = set_train - set_test
+        remove_test = set_test - set_train
+
+        remove = remove_train.union(remove_test)
+        print column, remove
+
+        def filter_cat(x):
+            if x in remove:
+                return np.nan
+            return x
+
+        joined[column] = joined[column].apply(lambda x: filter_cat(x), 1)
+        print 'unique =', joined[column].nunique()
+
+    joined[column] = pd.factorize(joined[column].values, sort=True)[0]
+
+train = joined[joined['loss'].notnull()].reset_index(drop=True)
+test = joined[joined['loss'].isnull()]
+
+shift = 200
+
+ids = test['id']
+X_train = train.drop(['loss', 'id'], 1).values
+X_test = test.drop(['loss', 'id'], 1).values
+y_train = np.log(train['loss'] + shift).values
+
+num_rounds = 300000
+RANDOM_STATE = 2016
+xgb_params = {
+    'min_child_weight': 1,
+    'eta': 0.01,
+    'colsample_bytree': 0.5,
+    'max_depth': 13,
+    'subsample': 0.8,
+    'alpha': 5,
+    'gamma': 1,
+    'silent': 1,
+    # 'base_score': 2,
+    'verbose_eval': 1,
+    'seed': RANDOM_STATE,
+    'nrounds': int(5903 / 0.8)
+}
+
+n_folds = 5
+num_train = len(y_train)
+num_test = test.shape[0]
+
+kf = KFold(n_folds, shuffle=True, random_state=RANDOM_STATE)
+
+
+class XgbWrapper(object):
+    def __init__(self, seed=0, params=None):
+        self.param = params
+        self.param['seed'] = seed
+        self.nrounds = params.pop('nrounds', 250)
+
+    def train(self, x_train, y_train):
+        dtrain = xgb.DMatrix(x_train, label=y_train)
+        self.gbdt = xgb.train(self.param, dtrain, self.nrounds, feval=evalerror, obj=logregobj)
+
+    def predict(self, x):
+        return self.gbdt.predict(xgb.DMatrix(x))
+
+
+def get_oof(clf):
+    oof_train = np.zeros((num_train,))
+    oof_test = np.zeros((num_test,))
+    oof_test_skf = np.empty((n_folds, num_test))
+
+    for i, (train_index, test_index) in enumerate(kf.split(X_train)):
+        print "Fold = ", i
+        x_tr = X_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = X_train[test_index]
+        y_te = y_train[test_index]
+
+        clf.train(x_tr, y_tr)
+
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(X_test)
+        print mean_absolute_error(np.exp(y_te), np.exp(oof_train[test_index]))
+        print
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+
+xg = XgbWrapper(seed=RANDOM_STATE, params=xgb_params)
+xg_oof_train, xg_oof_test = get_oof(xg)
+
+print
+print xg_oof_train[:, 0].shape, train.shape
+print xg_oof_train[:, 0]
+
+print("XG-CV: {}".format(mean_absolute_error(np.exp(y_train), np.exp(xg_oof_train))))
+
+oof_train = pd.DataFrame({'id': train['id'], 'xgb': (np.exp(xg_oof_train) - shift)[:, 0]})
+oof_train.to_csv('oof/xgb_train.csv', index=False)
+
+oof_test = pd.DataFrame({'id': test['id'], 'xgb': (np.exp(xg_oof_test) - shift)[:, 0]})
+oof_test.to_csv('oof/xgb_test.csv', index=False)
+
