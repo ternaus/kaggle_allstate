3810109b58e815355d491d48bdb7f6ae39acb3ce
Vladimir Iglovikov <iglovikov@gmail.com>
2016-11-15 07:11:11 -0800

    Added xgb with quadratic features 1107.53935

--- src/xgb_first_level.py
+++ src/xgb_first_level.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import KFold
 from pylab import *
 from tqdm import tqdm
 from sklearn.utils import shuffle
+import clean_data
 
 train = pd.read_csv('../data/train.csv')
 test = pd.read_csv('../data/test.csv')
@@ -24,42 +25,20 @@ def evalerror(preds, dtrain):
 def logregobj(preds, dtrain):
     labels = dtrain.get_label()
     con = 2
-    x =preds-labels
-    grad =con * x / (np.abs(x) + con)
-    hess =con**2 / (np.abs(x) + con)**2
+    x = preds - labels
+    grad = con * x / (np.abs(x) + con)
+    hess = con**2 / (np.abs(x) + con)**2
     return grad, hess
-
-
-for column in list(train.select_dtypes(include=['object']).columns):
-    if train[column].nunique() != test[column].nunique():
-        # Let's find extra categories...
-        set_train = set(train[column].unique())
-        set_test = set(test[column].unique())
-        remove_train = set_train - set_test
-        remove_test = set_test - set_train
-
-        remove = remove_train.union(remove_test)
-        print column, remove
-
-        def filter_cat(x):
-            if x in remove:
-                return np.nan
-            return x
-
-        joined[column] = joined[column].apply(lambda x: filter_cat(x), 1)
-        print 'unique =', joined[column].nunique()
-
-    joined[column] = pd.factorize(joined[column].values, sort=True)[0]
-
-train = joined[joined['loss'].notnull()].reset_index(drop=True)
-test = joined[joined['loss'].isnull()]
 
 shift = 200
 
-ids = test['id']
-X_train = train.drop(['loss', 'id'], 1).values
-X_test = test.drop(['loss', 'id'], 1).values
-y_train = np.log(train['loss'] + shift).values
+X_train, y_train, X_test, y_mean, X_test_id, X_train_id = clean_data.fancy(shift)
+
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
+
+print X_train.shape, X_test.shape
 
 RANDOM_STATE = 2016
 xgb_params = {
@@ -74,7 +53,7 @@ xgb_params = {
     # 'base_score': 2,
     'verbose_eval': 1,
     'seed': RANDOM_STATE,
-    'nrounds': 10000
+    'nrounds': 8000
 }
 
 n_folds = 5
@@ -85,21 +64,22 @@ kf = KFold(n_folds, shuffle=True, random_state=RANDOM_STATE)
 
 
 class XgbWrapper(object):
-    def __init__(self, seed=0, params=None):
+    def __init__(self, seed=2016, params=None):
         self.param = params
         self.param['seed'] = seed
         self.nrounds = params.pop('nrounds', 250)
 
-    def train(self, x_train, y_train, seed):
+    def train(self, x_train, y_train, seed, x_val, y_val):
         dtrain = xgb.DMatrix(x_train, label=y_train)
         self.param['seed'] = seed
-        self.gbdt = xgb.train(self.param, dtrain, self.nrounds, feval=evalerror, obj=logregobj)
+        dval = xgb.DMatrix(x_val, label=y_val)
+        watchlist = [(dtrain, 'train'), (dval, 'val')]
+        self.gbdt = xgb.train(self.param, dtrain, self.nrounds, watchlist, feval=evalerror, obj=logregobj, early_stopping_rounds=50)
 
     def predict(self, x):
         return self.gbdt.predict(xgb.DMatrix(x))
 
-
-nbags = 10
+nbags = 1
 
 
 def get_oof(clf):
@@ -117,8 +97,8 @@ def get_oof(clf):
 
         pred = np.zeros(x_te.shape[0])
 
-        for j in tqdm(range(nbags)):
-            clf.train(x_tr, y_tr, seed=RANDOM_STATE + i)
+        for j in range(nbags):
+            clf.train(x_tr, y_tr, RANDOM_STATE + i, x_te, y_te)
 
             pred += np.exp(clf.predict(x_te))
             pred_test += np.exp(clf.predict(X_test))
@@ -136,11 +116,11 @@ xg_oof_train, xg_oof_test = get_oof(xg)
 
 print("XG-CV: {}".format(mean_absolute_error(np.exp(y_train), xg_oof_train)))
 
-oof_train = pd.DataFrame({'id': train['id'], 'loss': (xg_oof_train - shift)})
-oof_train.to_csv('oof/xgb_train_t2.csv', index=False)
+oof_train = pd.DataFrame({'id': X_train_id, 'loss': (xg_oof_train - shift)})
+oof_train.to_csv('oof/xgb_train_t3.csv', index=False)
 
 xg_oof_test /= (n_folds * nbags)
 
-oof_test = pd.DataFrame({'id': test['id'], 'loss': (xg_oof_test - shift)})
-oof_test.to_csv('oof/xgb_test_t2.csv', index=False)
+oof_test = pd.DataFrame({'id': X_test_id, 'loss': (xg_oof_test - shift)})
+oof_test.to_csv('oof/xgb_test_t3.csv', index=False)
 
