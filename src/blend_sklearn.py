fadd2f51a5ec0155eb290a816cde1470a0cbbbea
Vladimir Iglovikov <iglovikov@gmail.com>
2016-11-20 15:50:14 -0800

    Added lr blend LB = 	1102.48338. CV = 1120.120000

--- src/blend_sklearn.py
+++ src/blend_sklearn.py
@@ -11,11 +11,12 @@ import sys
 sys.path += ['/home/vladimir/packages/xgboost/python-package']
 
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import Ridge
 from sklearn.metrics import make_scorer
 from pylab import *
+import clean_data
 
 
 def eval_f(x, y):
@@ -36,6 +37,11 @@ xgb_test_1 = pd.read_csv('oof/xgb_test_t.csv').rename(columns={'loss': 'xgb_loss
 xgb_train_2 = pd.read_csv('oof/xgb_train_t2.csv').rename(columns={'loss': 'xgb_loss_2'})
 xgb_test_2 = pd.read_csv('oof/xgb_test_t2.csv').rename(columns={'loss': 'xgb_loss_2'})
 
+xgb_train_3 = pd.read_csv('oof/xgb_train_t3.csv').rename(columns={'loss': 'xgb_loss_3'})
+xgb_test_3 = pd.read_csv('oof/xgb_test_t3.csv').rename(columns={'loss': 'xgb_loss_3'})
+
+xgb_train_4 = pd.read_csv('oof/xgb_train_t4.csv').rename(columns={'loss': 'xgb_loss_4'})
+xgb_test_4 = pd.read_csv('oof/xgb_test_t4.csv').rename(columns={'loss': 'xgb_loss_4'})
 
 nn_train = pd.read_csv('oof/NN_train.csv').rename(columns={'loss': 'nn_loss'})
 nn_test = pd.read_csv('oof/NN_test.csv').rename(columns={'loss': 'nn_loss'})
@@ -75,7 +81,9 @@ X_train = (train[['id', 'loss']]
            .merge(xgb_train_0, on='id')
            .merge(xgb_train, on='id')
            # .merge(xgb_train_1, on='id')
-            .merge(xgb_train_2, on='id')
+           #  .merge(xgb_train_2, on='id')
+            .merge(xgb_train_3, on='id')
+            .merge(xgb_train_4, on='id')
            .merge(nn_train, on='id')
            .merge(nn_train_1, on='id')
            .merge(nn_train_2, on='id')
@@ -92,7 +100,9 @@ X_test = (test[['id', 'cat1']]
           .merge(xgb_test_0, on='id')
           .merge(xgb_test, on='id')
           # .merge(xgb_test_1, on='id')
-            .merge(xgb_test_2, on='id')
+          #   .merge(xgb_test_2, on='id')
+          .merge(xgb_test_3, on='id')
+            .merge(xgb_test_4, on='id')
           .merge(nn_test, on='id')
           .merge(nn_test_1, on='id')
           .merge(nn_test_2, on='id')
@@ -108,6 +118,9 @@ X_test = (test[['id', 'cat1']]
 shift = 200
 
 y_train = np.log(X_train['loss'] + shift)
+
+X_train_id = X_train['id'].values
+X_test_id = X_test['id'].values
 
 X_train = X_train.drop(['id', 'loss'], 1).applymap(lambda x: np.log(x + shift)).values
 X_test = X_test.drop('id', 1).applymap(lambda x: np.log(x + shift)).values
@@ -129,53 +142,67 @@ RANDOM_STATE = 2016
 parameters = {'alpha': [0, 0.001, 0.005, 0.01, 0.1, 1, 10, 50, 100, 150, 180, 190, 200, 205, 210, 220, 240, 250, 260, 300, 500, 800, 1000]}
 
 n_folds = 5
-kf = KFold(n_folds, shuffle=True, random_state=RANDOM_STATE).get_n_splits(X_train)
+
+kf = StratifiedKFold(n_folds, shuffle=True, random_state=RANDOM_STATE)
 
 lr = Ridge(fit_intercept=False)
-clf = GridSearchCV(lr, parameters, n_jobs=-1, cv=kf, scoring=make_scorer(eval_f), iid=False)
+clf = GridSearchCV(lr, parameters, n_jobs=-1, cv=kf.get_n_splits(X_train, y_train), scoring=make_scorer(eval_f), iid=False)
 
 clf.fit(X_train, y_train)
 
 for i in clf.grid_scores_:
     print i
 
+num_train = X_train.shape[0]
+num_test = X_test.shape[0]
+
+classes = clean_data.classes(y_train, bins=100)
+
+
+nbags = 1
+
+
+def get_oof(clf):
+    pred_oob = np.zeros(X_train.shape[0])
+    pred_test = np.zeros(X_test.shape[0])
+
+    for i, (train_index, test_index) in enumerate(kf.split(classes, classes)):
+        print "Fold = ", i
+        x_tr = X_train[train_index]
+        y_tr = y_train[train_index]
+
+        x_te = X_train[test_index]
+        y_te = y_train[test_index]
+
+        pred = np.zeros(x_te.shape[0])
+
+        for j in range(nbags):
+            # x_tr, y_tr = shuffle(x_tr, y_tr, random_state=RANDOM_STATE + i + j)
+            clf.fit(x_tr, y_tr)
+
+            pred += np.exp(clf.predict(x_te))
+            pred_test += np.exp(clf.predict(X_test))
+
+        pred /= nbags
+        pred_oob[test_index] = pred
+        score = mean_absolute_error(np.exp(y_te), pred)
+        print('Fold ', i, '- MAE:', score)
+
+    return pred_oob, pred_test
+
 lr = Ridge(fit_intercept=False, alpha=0)
-lr.fit(X_train, y_train)
 
-test_pred = np.exp(lr.predict(X_test)) - shift
-submission = pd.DataFrame()
-submission['loss'] = test_pred
-submission['id'] = test_ids
-submission.to_csv('blen1.csv', index=False)
+lr_oof_train, lr_oof_test = get_oof(lr)
 
-# print clf.scorer_
-#
-#
-# print '[{datetime}] train set size = {size}'.format(datetime=str(datetime.datetime.now()), size=X_train.shape)
-#
-# print '[{datetime}] splitting'.format(datetime=str(datetime.datetime.now()))
-# xgtrain = xgb.DMatrix(X_train, label=y)
-#
-#
-# res = xgb.cv(params, xgtrain, num_boost_round=num_rounds, nfold=5, stratified=False,
-#              early_stopping_rounds=50, verbose_eval=1, show_stdv=True, maximize=False, feval=evalerror)
-#
-# best_nrounds = res.shape[0] - 1
-# cv_mean = res.iloc[-1, 0]
-# cv_std = res.iloc[-1, 1]
-# print('CV-Mean: {0}+{1}'.format(cv_mean, cv_std))
-#
-# model = xgb.train(params, xgtrain, int(best_nrounds / (1 - 1.0/5)), feval=evalerror)
-#
-# xgtest = xgb.DMatrix(X_test)
-#
-# print '[{datetime}] Creating submission'.format(datetime=str(datetime.datetime.now()))
-#
-#
-# prediction = np.exp(model.predict(xgtest)) - shift
-#
-# submission = pd.DataFrame()
-# submission['loss'] = prediction
-# submission['id'] = test_ids
-# submission.to_csv('xgb+NN.csv', index=False)
+print lr_oof_train.shape, X_train_id.shape
+
+print("LR-CV: {}".format(mean_absolute_error(np.exp(y_train), lr_oof_train)))
+
+oof_train = pd.DataFrame({'id': X_train_id, 'loss': (lr_oof_train - shift)})
+oof_train.to_csv('oof2/lr_train.csv', index=False)
+
+lr_oof_test /= n_folds
+
+oof_test = pd.DataFrame({'id': X_test_id, 'loss': (lr_oof_test - shift)})
+oof_test.to_csv('oof2/lr_test.csv', index=False)
 
