ce8761357f87264356c33d7fdf566a220bee5028
Vladimir Iglovikov <iglovikov@gmail.com>
2016-11-29 19:08:28 -0800

    Added et first level CV = 1191.44578333

--- src/ET_first_level.py
+++ src/ET_first_level.py
@@ -1,57 +1,27 @@
 from __future__ import division
 import pandas as pd
 from sklearn.ensemble import ExtraTreesRegressor
-
-
+import clean_data
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import KFold
 from pylab import *
 
-train = pd.read_csv('../data/train.csv')
-test = pd.read_csv('../data/test.csv')
-test['loss'] = np.nan
-joined = pd.concat([train, test])
-
-
-for column in list(train.select_dtypes(include=['object']).columns):
-    if train[column].nunique() != test[column].nunique():
-        # Let's find extra categories...
-        set_train = set(train[column].unique())
-        set_test = set(test[column].unique())
-        remove_train = set_train - set_test
-        remove_test = set_test - set_train
-
-        remove = remove_train.union(remove_test)
-        print column, remove
-
-        def filter_cat(x):
-            if x in remove:
-                return np.nan
-            return x
-
-        joined[column] = joined[column].apply(lambda x: filter_cat(x), 1)
-        print 'unique =', joined[column].nunique()
-
-    joined[column] = pd.factorize(joined[column].values, sort=True)[0]
-
-train = joined[joined['loss'].notnull()].reset_index(drop=True)
-test = joined[joined['loss'].isnull()]
-
 shift = 200
+X_train, y_train, X_test, y_mean, test_ids, train_ids = clean_data.fancy(shift=shift)
 
-ids = test['id']
-X_train = train.drop(['loss', 'id'], 1).values
-X_test = test.drop(['loss', 'id'], 1).values
-y_train = np.log(train['loss'] + shift).values
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
 
 num_rounds = 300000
 RANDOM_STATE = 2016
 
-n_folds = 5
-num_train = len(y_train)
-num_test = test.shape[0]
+n_folds = 10
+num_train = X_train.shape[0]
+num_test = X_test.shape[0]
 
-kf = KFold(n_folds, shuffle=True, random_state=RANDOM_STATE)
+classes = clean_data.classes(y_train, bins=100)
+kf = StratifiedKFold(n_folds, shuffle=True, random_state=RANDOM_STATE)
 
 et_params = {
     'n_jobs': -1,
@@ -79,7 +49,7 @@ def get_oof(clf):
     oof_test = np.zeros((num_test,))
     oof_test_skf = np.empty((n_folds, num_test))
 
-    for i, (train_index, test_index) in enumerate(kf.split(X_train)):
+    for i, (train_index, test_index) in enumerate(kf.split(classes, classes)):
         print "Fold = ", i
         x_tr = X_train[train_index]
         y_tr = y_train[train_index]
@@ -99,15 +69,12 @@ def get_oof(clf):
 et = SklearnWrapper(clf=ExtraTreesRegressor, seed=RANDOM_STATE, params=et_params)
 xg_oof_train, xg_oof_test = get_oof(et)
 
-print
-print xg_oof_train[:, 0].shape, train.shape
-print xg_oof_train[:, 0]
 
 print("et-CV: {}".format(mean_absolute_error(np.exp(y_train), np.exp(xg_oof_train))))
 
-oof_train = pd.DataFrame({'id': train['id'], 'loss': (np.exp(xg_oof_train) - shift)[:, 0]})
+oof_train = pd.DataFrame({'id': train_ids, 'loss': (np.exp(xg_oof_train) - shift)[:, 0]})
 oof_train.to_csv('oof/et_train.csv', index=False)
 
-oof_test = pd.DataFrame({'id': test['id'], 'loss': (np.exp(xg_oof_test) - shift)[:, 0]})
+oof_test = pd.DataFrame({'id': test_ids, 'loss': (np.exp(xg_oof_test) - shift)[:, 0]})
 oof_test.to_csv('oof/et_test.csv', index=False)
 
